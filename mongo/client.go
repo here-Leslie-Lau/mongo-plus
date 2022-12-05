814846bd453cec04c19de24a8275b760bff32e8e
leslie-lau <804738464@qq.com>
2022-12-05 17:44:44 +0800

    init client

--- mongo/client.go
+++ mongo/client.go
@@ -1,5 +1,43 @@
 package mongo
 
-type client struct{
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+type Client struct {
 	cfg *config
+	cli *mongo.Client
+	db  *mongo.Database
+}
+
+func NewClient(opts ...Option) (*Client, func()) {
+	cfg := &config{}
+	for _, opt := range opts {
+		opt(cfg)
+	}
+
+	ctx := context.Background()
+
+	option := cfg.getOption()
+
+	// connect
+	cli := &Client{cfg: cfg}
+	var err error
+	cli.cli, err = mongo.Connect(ctx, option)
+	if err != nil {
+		panic(err)
+	}
+	// test ping
+	if err := cli.cli.Ping(ctx, readpref.Primary()); err != nil {
+		panic(errors.Wrap(err, "mongo ping fail"))
+	}
+	cli.db = cli.cli.Database(cfg.Database)
+
+	return cli, func() {
+		_ = cli.cli.Disconnect(context.Background())
+	}
 }
--- mongo/config.go
+++ mongo/config.go
@@ -1,5 +1,9 @@
 package mongo
 
+import (
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 type config struct {
 	// 最大连接数
 	MaxPoolSize uint64
@@ -7,8 +11,22 @@ type config struct {
 	Username string
 	// mongodb密码
 	Password string
+	// 要连接的数据库名
+	Database string
 	// mongodb连接的url
 	Addr []string
+}
+
+func (cfg *config) getOption() *options.ClientOptions {
+	opt := options.Client()
+	opt.Hosts = cfg.Addr
+	opt.Auth = &options.Credential{
+		Username: cfg.Username,
+		Password: cfg.Password,
+	}
+	opt.SetMaxPoolSize(cfg.MaxPoolSize)
+
+	return opt
 }
 
 type Option func(*config)
@@ -22,6 +40,12 @@ func WithUsername(username string) Option {
 func WithPassword(passwd string) Option {
 	return func(cfg *config) {
 		cfg.Password = passwd
+	}
+}
+
+func WithDatabase(dbName string) Option {
+	return func(cfg *config) {
+		cfg.Database = dbName
 	}
 }
 
