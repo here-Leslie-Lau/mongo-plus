8aa0acd9ee9dea16df450beb7923c6ef913a42ac
leslie-lau <804738464@qq.com>
2023-08-29 16:41:33 +0800

    feat: init commit pkg cmds
    create pkg cmds and new mongodb conn in cmds

---
+++ cmds/cmds.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/here-Leslie-Lau/mongo-plus/mongo"
+)
+
+var (
+	username string
+	pass     string
+	databse  string
+	addr     string
+)
+
+func init() {
+	flag.StringVar(&username, "u", "", "mongodb username")
+	flag.StringVar(&pass, "p", "", "mongodb password")
+	flag.StringVar(&databse, "d", "", "mongodb database")
+	flag.StringVar(&addr, "addr", "localhost:27017", "mongodb address and port")
+}
+
+func main() {
+	flag.Parse()
+
+	if username == "" || pass == "" || databse == "" {
+		fmt.Println("u, p, d is required")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	opts := []mongo.Option{
+		mongo.WithUsername(username),
+		mongo.WithPassword(pass),
+		mongo.WithDatabase(databse),
+		mongo.WithAddr(addr),
+		mongo.WithMaxPoolSize(2),
+	}
+	_, f := mongo.NewConn(opts...)
+	defer f()
+}
